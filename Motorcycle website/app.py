d0d81f3fd8b9d082da796f1bdb283b4d6a921e1b
Xavier Reid <your.email@example.com>
2026-02-25 12:26:54 +1300

    nkmmmm

--- Motorcycle website/app.py
+++ Motorcycle website/app.py
@@ -1,10 +1,39 @@
-from flask import Flask
+from flask import Flask, g
+import sqlite3
 
+DATABASE = 'database.db'
+
+#initalise app
 app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
 
 @app.route('/')
 def home():
-    return "Hello Again!"
+    #home page hust the ID, Maker, Model and Image URL
+    sql = """   
+                SELECT Bikes.BikeID,Makers.Name,Bikes.Model,Bikes.ImageURL
+                FROM Bikes
+                Join Makers ON Makers.MakerID=Bikes.MakerID;"""
+    results = query_db(sql)
+    return str(results)
 
 if __name__ == "__main__":
     app.run(debug=True)
