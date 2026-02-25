a0dba9cb3a50bb705738cbd9b3996ac999520d3b
Xavier Reid <your.email@example.com>
2026-02-26 10:00:49 +1300

    hthrh

--- Motorcycle website/app.py
+++ Motorcycle website/app.py
@@ -35,5 +35,14 @@ def home():
     results = query_db(sql)
     return str(results)
 
+@app.route("/bike/<int:id>")
+def bike(id):
+    #just one bike based on the id
+    sql = """SELECT * FROM Bikes 
+    JOIN Makers ON Makers.MakerID=Bikes.MakerID
+    WHERE Bikes.BikeID = ?;"""
+    result = query_db(sql,(id,),True)
+    return str(result)
+
 if __name__ == "__main__":
     app.run(debug=True)
